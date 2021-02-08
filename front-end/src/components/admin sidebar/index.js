1235774290555bc912c924808bd27dd633dac556
ederusena <eder.sena@live.com>
2021-02-08 20:19:45 -0300

    Menu Sidebar

--- front-end/src/components/admin sidebar/index.js
+++ front-end/src/components/admin sidebar/index.js
@@ -1,36 +1,102 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import FaceIcon from '@material-ui/icons/Face';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
-import './index.css';
+const drawerWidth = 240;
 
-const AdminSideBar = () => (
-  <aside className="adminSideBar admin-side-bar-container">
-    <ul>
-      <li className="adminTitle">Trybeer</li>
-      <li className="adminMenuBtnContainer">
-        <Link to="/admin/orders" className="adminMenuBtn" data-testid="side-menu-item-orders">
-          Pedidos
-        </Link>
-      </li>
-      <li className="adminMenuBtnContainer">
-        <Link to="/admin/profile" className="adminMenuBtn" data-testid="side-menu-item-profile">
-          Perfil
-        </Link>
-      </li>
-    </ul>
-    <ul>
-      <li className="adminMenuBtnContainer">
-        <Link
-          to="/"
-          className="adminMenuBtn"
-          data-testid="side-menu-item-logout"
-          onClick={ () => { localStorage.removeItem('token'); } }
-        >
-          Sair
-        </Link>
-      </li>
-    </ul>
-  </aside>
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
 
-export default AdminSideBar;
+export default function PermanentDrawerLeft() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Admin Perfil
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          <ListItem button component={Link} to="/admin/orders">
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary='Pedidos' />
+          </ListItem>
+          <ListItem button component={Link} to="/admin/profile">
+            <ListItemIcon>
+              <FaceIcon />
+            </ListItemIcon>
+            <ListItemText primary='Perfil'/>
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <MailIcon />
+            </ListItemIcon>
+            <ListItemText primary='Mensagens' />
+          </ListItem>
+
+        </List>
+        <Divider />
+        <List>
+            <ListItem
+              button
+              onClick={() => localStorage.removeItem('token')}
+            >
+              <ListItemIcon><ExitToAppIcon /></ListItemIcon>
+              <ListItemText primary='Logout' />
+            </ListItem>
+        </List>
+      </Drawer>
+    </div>
+  );
+}
