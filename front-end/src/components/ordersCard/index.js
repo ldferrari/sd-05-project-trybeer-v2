7c8a7b822b35ee24c0e9582fe42f805bb451a0f4
pauloricardoz <trybe.przt@gmail.com>
2021-02-11 03:11:15 -0300

    refactoring ordersCards to 20 lines

--- front-end/src/components/ordersCard/index.js
+++ front-end/src/components/ordersCard/index.js
@@ -5,13 +5,25 @@ import './index.css';
 import { Link } from 'react-router-dom';
 import AppContext from '../../context/AppContext';
 
-const OrderCard = (props) => {
-  const { order, index } = props;
-  const { id, sale_date: saleDate } = order;
-  const { setGlobalData } = useContext(AppContext);
-  useEffect(() => {
-    setGlobalData((state) => ({ ...state, [id]: saleDate }));
-  }, [id, saleDate, setGlobalData]);
+function OrderCardTitle(index, id) {
+  return (
+    <p data-testid={ `${index}-order-number` } className="orderCardTitle">
+      {`Pedido ${id}`}
+    </p>
+  );
+}
+function OrderCardDate(index, saleDate) {
+  return <p data-testid={ `${index}-order-date` }>{saleDate}</p>;
+}
+function OrderCardTotalValue(index, order) {
+  return (
+    <p data-testid={ `${index}-order-total-value` }>
+      { `R$ ${order.total_price.toString().replace('.', ',')}` }
+    </p>
+  );
+}
+
+function OrderCardFinalCard(id, index, saleDate, order) {
   return (
     <div className="oCard">
       <Link
@@ -20,18 +32,21 @@ const OrderCard = (props) => {
         key={ id }
         data-testid={ `${index}-order-card-container"` }
       >
-        <p data-testid={ `${index}-order-number` } className="orderCardTitle">
-          { `Pedido ${id}` }
-        </p>
-        <p data-testid={ `${index}-order-date` }>
-          { saleDate }
-        </p>
-        <p data-testid={ `${index}-order-total-value` }>
-          { `R$ ${order.total_price.toString().replace('.', ',')}` }
-        </p>
+        {OrderCardTitle(index, id)}
+        {OrderCardDate(index, saleDate)}
+        {OrderCardTotalValue(index, order)}
       </Link>
     </div>
   );
+}
+const OrderCard = (props) => {
+  const { order, index } = props;
+  const { id, sale_date: saleDate } = order;
+  const { setGlobalData } = useContext(AppContext);
+  useEffect(() => {
+    setGlobalData((state) => ({ ...state, [id]: saleDate }));
+  }, [id, saleDate, setGlobalData]);
+  return OrderCardFinalCard(id, index, saleDate, order);
 };
 
 export default OrderCard;
--- front-end/src/components/productCard/index.js
+++ front-end/src/components/productCard/index.js
@@ -3,68 +3,102 @@ import PropTypes from 'prop-types';
 import AppContext from '../../context/AppContext';
 import './index.css';
 
-const Card = (props) => {
-  const { cart, setCart } = useContext(AppContext);
-
-  const { product } = props;
-
-  const exists = cart.find((produto) => produto.id === product.id);
-
-  const minusOne = () => {
+function minusOneFunction(exists, setCart, cart, product) {
+  return () => {
     const zero = 0;
-    // const exists = cart.find((produto) => produto.id === product.id);
-    const newProduct = (e) => ({ ...e, quantity: e.quantity <= zero ? zero : e.quantity - 1 });
+    const newProduct = (e) => ({
+      ...e,
+      quantity: e.quantity <= zero ? zero : e.quantity - 1,
+    });
     if (exists) {
       setCart(cart.map((e) => (e.id === product.id ? newProduct(e) : e)));
     }
     return null;
   };
-
-  const plusOne = () => {
+}
+function plusOneFunction(exists, setCart, cart, product) {
+  return () => {
     const um = 1;
-    return exists
-      ? setCart(
-        cart.map((e) => (e.id === product.id ? { ...e, quantity: e.quantity + 1 } : e)),
-      )
-      : setCart(
-        [
-          ...cart, {
-            id: product.id, name: product.name, price: product.price, quantity: um,
-          },
-        ],
-      );
+    if (exists) {
+      return setCart(cart
+        .map((e) => (e.id === product.id ? { ...e, quantity: e.quantity + 1 } : e)));
+    }
+    return setCart([
+      ...cart,
+      {
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        quantity: um,
+      },
+    ]);
   };
+}
+function ImageProduct(product) {
+  return (
+    <img
+      src={ product.url_image }
+      alt={ product.name }
+      data-testid={ `${product.id - 1}-product-img` }
+    />
+  );
+}
+function ProductPrice(product) {
+  return (
+    <h4 data-testid={ `${product.id - 1}-product-price` }>
+      { `R$ ${product.price.toString().replace('.', ',')}` }
+    </h4>
+  );
+}
+function ButtonMinus(minusOne, product) {
+  return (
+    <button
+      type="button"
+      className="maisOuMenos"
+      data-testid={ `${product.id - 1}-product-minus` }
+      onClick={ () => minusOne() }
+    >
+      -
+    </button>
+  );
+}
+function ButtonPlus(plusOne, product) {
+  return (
+    <button
+      type="button"
+      className="maisOuMenos"
+      data-testid={ `${product.id - 1}-product-plus` }
+      onClick={ () => plusOne() }
+    >
+      +
+    </button>
+  );
+}
+
+function ControlQuantity(exists, product, minusOne, plusOne) {
   const zero = 0;
   return (
+    <div className="cardBottom">
+      {ButtonMinus(minusOne, product)}
+      <p data-testid={ `${product.id - 1}-product-qtd` }>
+        {exists ? exists.quantity : zero}
+      </p>
+      {ButtonPlus(plusOne, product)}
+    </div>
+  );
+}
+const Card = (props) => {
+  const { cart, setCart } = useContext(AppContext);
+  const { product } = props;
+  const exists = cart.find((produto) => produto.id === product.id);
+  const minusOne = minusOneFunction(exists, setCart, cart, product);
+  const plusOne = plusOneFunction(exists, setCart, cart, product);
+  return (
     <div className="card" key={ product.name }>
-      <img
-        src={ product.url_image }
-        alt={ product.name }
-        data-testid={ `${product.id - 1}-product-img` }
-      />
-      <p data-testid={ `${product.id - 1}-product-name` }>{ product.name }</p>
-      <h4 data-testid={ `${product.id - 1}-product-price` }>
-        {`R$ ${product.price.toString().replace('.', ',')}` }
-      </h4>
-      <div className="cardBottom">
-        <button
-          type="button"
-          className="maisOuMenos"
-          data-testid={ `${product.id - 1}-product-minus` }
-          onClick={ () => minusOne() }
-        >
-          -
-        </button>
-        <p data-testid={ `${product.id - 1}-product-qtd` }>{ exists ? exists.quantity : zero }</p>
-        <button
-          type="button"
-          className="maisOuMenos"
-          data-testid={ `${product.id - 1}-product-plus` }
-          onClick={ () => plusOne() }
-        >
-          +
-        </button>
-      </div>
+      {ImageProduct(product)}
+      <p data-testid={ `${product.id - 1}-product-name` }>{product.name}</p>
+      {ProductPrice(product)}
+      {ControlQuantity(exists, product, minusOne, plusOne)}
     </div>
   );
 };
