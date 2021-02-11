ffa6bb478a2550d93ca96450510b63538f739756
pauloricardoz <trybe.przt@gmail.com>
2021-02-11 03:13:43 -0300

    refactoring header to 20 lines

--- front-end/src/components/header/index.js
+++ front-end/src/components/header/index.js
@@ -5,20 +5,25 @@ import logo from '../../images/uai2.png';
 
 import './index.css';
 
+function BurgerButton(setDisplay, display) {
+  return (
+    <button
+      type="button"
+      className="burguerBtn"
+      data-testid="top-hamburguer"
+      onClick={ () => { setDisplay(!display); } }
+    >
+      &#9776;
+    </button>
+  )
+}
+
 const Header = ({ children }) => {
   const [display, setDisplay] = useState(false);
-
   return (
     <header>
       <div className="header">
-        <button
-          type="button"
-          className="burguerBtn"
-          data-testid="top-hamburguer"
-          onClick={ () => { setDisplay(!display); } }
-        >
-          &#9776;
-        </button>
+        {BurgerButton(setDisplay, display)}
         <h2 data-testid="top-title" className="title">{children}</h2>
         <img src={ logo } className="logoHeader" alt="logo" />
       </div>
