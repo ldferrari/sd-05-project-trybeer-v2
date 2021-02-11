32304695375423c5f5da93fc84f6b15c9b6af4ff
pauloricardoz <trybe.przt@gmail.com>
2021-02-11 03:27:09 -0300

    refactoring cartItem to 20 lines

--- front-end/src/components/cartItem/index.js
+++ front-end/src/components/cartItem/index.js
@@ -3,49 +3,66 @@ import PropTypes from 'prop-types';
 import AppContext from '../../context/AppContext';
 import './index.css';
 
-
-function RenderFinal(item, index, exists, exclude) {
+function CartItemQuantity(index, exists) {
   const zero = 0;
-  const two = 2;
   return (
-    <div className="cartItem" key={ item.name }>
-      <p data-testid={ `${index}-product-qtd-input` }>
-        {exists.quantity ? exists.quantity : zero}
-      </p>
-      <p data-testid={ `${index}-product-name` }>{item.name}</p>
-      <p data-testid={ `${index}-product-unit-price` }>
-        { `(R$ ${item.price.toString().replace('.', ',')} un)` }
-      </p>
-      <p data-testid={ `${index}-product-total-value` }>
-        { `R$ ${(item.price * item.quantity).toFixed(two).replace('.', ',')}` }
-      </p>
-      <button
-        type="button"
-        data-testid={ `${index}-removal-button` }
-        onClick={ () => exclude() }
-      >
-        X
-      </button>
-    </div>
+    <p data-testid={ `${index}-product-qtd-input` }>
+      {exists.quantity ? exists.quantity : zero}
+    </p>
   );
 }
-const CartItem = (props) => {
-  const { cart, setCart } = useContext(AppContext);
-  const { item, index } = props;
-  const exists = cart.find((produto) => produto.id === item.id);
-  const zero = 0;
+function CartItemUnitPrice(index, item) {
+  return (
+    <p data-testid={ `${index}-product-unit-price` }>
+      {`(R$ ${item.price.toString().replace('.', ',')} un)`}
+    </p>
+  );
+}
+function CartItemTotalPrice(index, item) {
   const two = 2;
-  if (!exists || exists.quantity === zero) {
-    return null;
-  }
-  const exclude = () => {
+  return (
+    <p data-testid={ `${index}-product-total-value` }>
+      {`R$ ${(item.price * item.quantity).toFixed(two).replace('.', ',')}`}
+    </p>
+  );
+}
+function CartItemRemovalButton(index, exclude) {
+  return (
+    <button
+      type="button"
+      data-testid={ `${index}-removal-button` }
+      onClick={ () => exclude() }
+    >
+      X
+    </button>
+  );
+}
+function excludeFunction(exists, cart, item, setCart) {
+  const zero = 0;
+  return () => {
     const itemZero = (e) => ({ ...e, quantity: zero });
     if (exists) {
       setCart(cart.map((e) => (e.id === item.id ? itemZero(e) : e)));
     }
     return null;
   };
-  return RenderFinal(item, index, exists, exclude);
+}
+const CartItem = (props) => {
+  const { cart, setCart } = useContext(AppContext);
+  const { item, index } = props;
+  const exists = cart.find((produto) => produto.id === item.id);
+  const zero = 0;
+  if (!exists || exists.quantity === zero) { return null; }
+  const exclude = excludeFunction(exists, cart, item, setCart);
+  return (
+    <div className="cartItem" key={ item.name }>
+      {CartItemQuantity(index, exists)}
+      <p data-testid={ `${index}-product-name` }>{item.name}</p>
+      {CartItemUnitPrice(index, item)}
+      {CartItemTotalPrice(index, item)}
+      {CartItemRemovalButton(index, exclude)}
+    </div>
+  );
 };
 
 export default CartItem;
