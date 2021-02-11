d1f762dd807ba49ace49f0e9eb53c7f68e5d945b
Larissa Palombo <larissampalombo@gmail.com>
2021-02-11 18:29:33 -0300

    lint

--- front-end/src/components/client/ClientMenu.js
+++ front-end/src/components/client/ClientMenu.js
@@ -1,55 +1,62 @@
-// ref https://medium.com/@renan.serverti/aprenda-frontend-como-criar-um-menu-toggle-responsivo-com-css-4f7f7a2ac07a
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import '../../css/ClientMenu.css';
 
+function hamburger(openClose) {
+  return (
+    <label className="top-hamburguer" data-testid="top-hamburguer" htmlFor="check">
+      &#9776;
+      <input type="checkbox" id="check" onChange={ () => openClose() } />
+    </label>
+  );
+}
+
+function menuTitle(title) {
+  return (
+    <h1 className="top-title" data-testid="top-title">
+      {title}
+    </h1>
+  );
+}
+
+function nav() {
+  return (
+    <nav className="side-menu">
+      <Link className="btn-side btn-menu" to="/products">
+        Produtos
+      </Link>
+      <Link className="btn-side btn-menu" to="/orders">
+        Meus Pedidos
+      </Link>
+      <Link className="btn-side btn-menu" to="/profile">
+        Meu Perfil
+      </Link>
+      <Link className="btn-side btn-menu" to="/login">Sair</Link>
+      <Link className="btn-side btn-menu" data-testid="side-menu-chat" to="/chat">
+        Conversar com a loja
+      </Link>
+    </nav>
+  );
+}
+
 function ClientMenu(props) {
   const [click, setClick] = useState(false);
   const { title } = props;
-
   function openClose() {
     if (document.getElementById('check').checked) {
       setClick(true);
-    }
-    if (!document.getElementById('check').checked) {
+    } else {
       setClick(false);
     }
   }
-
-  const left = click ? '0' : '-40%';
-  const display = click ? 'block' : 'none';
-
+  const l = click ? '0' : '-40%';
+  const d = click ? 'block' : 'none';
   return (
     <header className="menu-superior">
-      <label className="top-hamburguer" data-testid="top-hamburguer" htmlFor="check">
-        &#9776;
-        <input type="checkbox" id="check" onChange={() => openClose()} />
-      </label>
-      <h1 className="top-title" data-testid="top-title">
-        {title}
-      </h1>
-      <div className="side-menu-container" style={{ left, display }}>
-        <nav className="side-menu">
-          <div className="btn-menu">
-            <Link className="btn-side" data-testid="side-menu-item-products" to="/products">
-              Produtos
-            </Link>
-            <Link className="btn-side" data-testid="side-menu-item-my-orders" to="/orders">
-              Meus Pedidos
-            </Link>
-            <Link className="btn-side" data-testid="side-menu-item-my-profile" to="/profile">
-              Meu Perfil
-            </Link>
-            <Link className="btn-side" data-testid="side-menu-item-logout" to="/login">
-              Sair
-            </Link>
-            <Link className="btn-side" data-testid="side-menu-chat" to="/chat">
-              Conversar com a loja
-            </Link>
-          </div>
-        </nav>
-      </div>
+      {hamburger(openClose)}
+      {menuTitle(title)}
+      <div className="menu-container" style={ { l, d } }>{nav()}</div>
     </header>
   );
 }
