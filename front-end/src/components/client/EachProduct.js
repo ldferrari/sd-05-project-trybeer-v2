f6e5c94e18f09cd80a9dbb5f832586ac512ad94a
virginia-duca <virginiambd.bio@gmail.com>
2021-02-17 15:40:05 -0300

    salvando arquivos

--- front-end/src/components/client/EachProduct.js
+++ front-end/src/components/client/EachProduct.js
@@ -3,67 +3,74 @@ import PropTypes from 'prop-types';
 import TrybeerContext from '../../context/TrybeerContext';
 import { saveProductsMore, saveProductsLess } from '../../services/localStorage';
 
+const initialCount = 0;
+
+const renderImage = (index) => {
+  <img
+    data-testid={ `${index}-product-img` }
+    src={ product.url_image }
+    alt=""
+    width="100"
+    height="60"
+  />
+}
+
+const renderPrice = (index) => {
+  <p data-testid={ `${index}-product-price` }>
+    {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+      product.price,
+    )}
+  </p>
+}
+
+const renderProductName = (product, index) => {
+  <p key={ product.id } data-testid={ `${index}-product-name` }>
+    {product.name}
+  </p>
+}
+
+const buttonLess = (product, index, callback) => {
+  <button
+    type="button"
+    key={ product.id }
+    data-testid={ `${index}-product-minus` }
+    onClick={ () => callback() }
+    >
+    -
+  </button>
+}
+
+const buttonMore = (product, index, callback) => {
+  <button
+  type="button"
+  key={ product.id }
+  data-testid={ `${index}-product-plus` }
+  onClick={ () => callback() }
+    >
+    +
+  </button>
+}
+
 function EachProduct(props) {
   const { product, index } = props;
-  const initialCount = 0;
   const [countProduct, setCountProduct] = useState(initialCount);
   const { totalPrice, setTotalPrice } = useContext(TrybeerContext);
-
   const oneLess = async () => {
     if (countProduct > initialCount) {
-      setCountProduct(countProduct - 1);
-      setTotalPrice(totalPrice - product.price);
-      saveProductsLess(product);
-      setTotalPrice(totalPrice - Number(product.price));
-    }
-  };
+      setCountProduct(countProduct - 1), setTotalPrice(totalPrice - product.price);
+      saveProductsLess(product), setTotalPrice(totalPrice - Number(product.price))}};
   const oneMore = async () => {
-    setCountProduct(countProduct + 1);
-    setTotalPrice(totalPrice + Number(product.price));
+    setCountProduct(countProduct + 1), setTotalPrice(totalPrice + Number(product.price));
     saveProductsMore(product);
-    localStorage.setItem('totalPrice', totalPrice + Number(product.price));
-  };
-
+    localStorage.setItem('totalPrice', totalPrice + Number(product.price))};
   return (
     <div className="eachCard">
-      <p data-testid={ `${index}-product-price` }>
-        {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
-          product.price,
-        )}
-      </p>
-      <img
-        data-testid={ `${index}-product-img` }
-        src={ product.url_image }
-        alt=""
-        width="100"
-        height="60"
-      />
-      <p key={ product.id } data-testid={ `${index}-product-name` }>
-        {product.name}
-      </p>
-      <div className="controlQty">
-        <button
-          type="button"
-          key={ product.id }
-          data-testid={ `${index}-product-minus` }
-          onClick={ () => oneLess() }
-        >
-          -
-        </button>
+      {renderPrice(index), renderImage(index), renderProductName(product, index)}
+      <div className="controlQty"> {buttonLess(product, index, oneLess)}
         <p data-testid={ `${index}-product-qtd` } id={ `${index}-price` }>
-          {countProduct}
-        </p>
-        <button
-          type="button"
-          key={ product.id }
-          data-testid={ `${index}-product-plus` }
-          onClick={ () => oneMore() }
-        >
-          +
-        </button>
-      </div>
-    </div>
-  );
+          {countProduct}</p> {buttonMore(product, index, oneMore)}
+      </div></div>
+    )
 }
 
 EachProduct.propTypes = {
