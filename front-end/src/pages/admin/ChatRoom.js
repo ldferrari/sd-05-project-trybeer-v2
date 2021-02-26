72ff566024e3fddda789ba8760525dcbeef44679
Lucascv <lucascv333@gmail.com>
2021-02-26 16:06:15 -0300

    teste

--- front-end/src/pages/admin/ChatRoom.js
+++ front-end/src/pages/admin/ChatRoom.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 // import { Link } from 'react-router-dom';
 // import { all } from 'sequelize/types/lib/operators'; ?
+import AdminMenu from '../../components/admin/AdminMenu';
 import AdminChat from '../../components/admin/AdminChat';
 
 const { getAllMessages } = require('../../services/fetchMongo');
@@ -20,12 +21,20 @@ function ChatRoom() {
     fetchData();
   }, []);
 
-  if (conversa) return <AdminChat email={ email } />;
+  if (conversa) {
+    return (
+      <div>
+        <AdminMenu />
+        <AdminChat email={ email } />
+      </div>
+    );
+  }
 
   if (!allChats || allChats.length < 1) {
     return (
       <section>
         <h2>Conversas</h2>
+        <AdminMenu />
         <h3 data-testid="text-for-no-conversation">Nenhuma conversa por aqui</h3>
       </section>
     );
@@ -34,6 +43,7 @@ function ChatRoom() {
   if (allChats.length > 0) {
     return (
       <section>
+        <AdminMenu />
         <h2>Conversas</h2>
         {console.log(conversa)}
         {allChats.map((chat, index) => (
