c75d980dd68a5448a3f79a5be874f146dd4ef58f
César Alves <alvescesar@hotmail.com>
2021-02-28 03:10:56 -0300

    Deleting to avoid merge conflicts

--- back-end/index.js
+++ back-end/index.js
@@ -23,20 +23,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const chatModel = require('./chatModel/chat');
-
 app.use('/login', loginController);
 app.use('/register', registerController);
 app.use('/users', userController);
 app.use('/products', productController);
 app.use('/orders', salesController);
-app.get('/chat', async (req, res) => {
-  const { nickname } = req.query;
-  const status200 = 200;
-  const messages = await chatModel.getUserChatHistory(nickname);
-
-  res.status(status200).json(messages);
-});
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
@@ -44,16 +35,3 @@ const port = 3001;
 const PORT = process.env.PORT || port;
 server.listen(PORT, () => console.log('Tô na escuta'));
 
-io.on('connection', async (socket) => {
-  console.log(`Guest id: ${socket.id} just joined the chat!`);
-
-  socket.on('message', async ({ nickname, message, role }) => {
-    const timestamp = moment(new Date()).format('hh:mm');
-    await chatModel.createMessage({ nickname, message, timestamp, role });
-    io.emit('message', { nickname, timestamp, message });
-  });
-
-  socket.on('disconnect', () => {
-    console.log(`Guest id: ${socket.id} just left.`);
-  });
-});
